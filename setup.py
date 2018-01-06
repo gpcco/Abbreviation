05b80c1dc9782dd083ddaa87532b07bfc10ce79e
emretekinalp <e.tekinalp@icloud.com>
2018-01-05 23:51:57 -0800

    implemented initial folder structure including setup.py pypi conform

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='abbreviation',
+      version='0.1',
+      description='tool to abbreviate any string',
+      url='https://github.com/gpcco/abbreviation.git',
+      author='Emre Tekinalp',
+      author_email='e.tekinalp@icloud.com',
+      license='MIT',
+      packages=['abbreviation'],
+      zip_safe=False)
